e1a61de916f80d2b87662c514200d072a811c287
Shaelyn26 <shaelynb0405@gmail.com>
2025-03-08 15:51:01 +0000

    added two tests

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const sum = function(a,b) {
+    let result = a + b 
+    return result
+}
+
+
 const fromEuroToDollar = function(valueInEuro) {
     let valueInDollar = valueInEuro * 1.07;
 
@@ -5,17 +11,19 @@ const fromEuroToDollar = function(valueInEuro) {
 }
 
 const fromDollarToYen = function(valueInDollar) {
-    let valueInYen = valueInDollar * 156.5;
+    let valueInEuro = valueInDollar / 1.07;
+    let valueInYen = valueInEuro * 156.5;
 
     return valueInYen;
 }
 
 const fromYenToPound = function(valueInYen) {
-    let valueInPound = valueInYen * 0.87;
+    let valueInEuro = valueInYen / 156.5;
+    let valueInPound = valueInEuro * 0.87;
 
     return valueInPound;
 }
 
 
 
-module.exports = {fromEuroToDollar, fromDollarToYen, fromYenToPound };
+module.exports = {sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
--- test.js
+++ test.js
@@ -16,3 +16,23 @@ test("One euro should be 1.07 dollars", function () {
 
     expect(fromEuroToDollar(3.5)).toBe(3.745);
 })
+
+test("One dollar should be 146.26 yen", function (){
+    const { fromDollarToYen } = require('./app.js');
+
+    const dollar = fromDollarToYen(1);
+
+    const expected = (1 / 1.07) * 156.5;
+
+    expect(fromDollarToYen(1)).toBe(expected);
+})
+
+test("One Yen should be .005 pounds", function(){
+    const { fromYenToPound } = require('./app.js');
+
+    const Yen = fromYenToPound(1);
+
+    const expected = (1 / 156.5) * .87;
+
+    expect(fromYenToPound(1)).toBe(expected);
+})
